1c0126f7382d6b8e3a9ac54a7848554500a9cd6c
abdUlrahman <bbdw69243@gmail.com>
2024-12-28 02:48:33 +0300

    create prodects table but without photo

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserInfo;
-
+use App\Http\Controllers\ProductController;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
@@ -14,3 +15,4 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'
 ])->middleware('auth:sanctum');
 Route::post('/upload_profile_image', [UserInfo::class, 'uploadImage'])->middleware('auth:sanctum');
+Route::post('/products', [ProductController::class, 'store']);
