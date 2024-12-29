16b308d8d6480f27f5bae0ce24869d5a1c418b65
abdUlrahman <bbdw69243@gmail.com>
2024-12-29 21:26:52 +0300

    .

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,42 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cart;
-use App\Models\Product;
-
 class CartController extends Controller
 {
+    //
     public function addToCart(Request $request)
-    {
-        $validated = $request->validate([
-            'product_id' => 'required|exists:products,id',
-            'quantity' => 'required|integer|min:1',
+{
+    $validated = $request->validate([
+        'product_id' => 'required|exists:products,id',
+        'quantity' => 'required|integer|min:1',
+    ]);
+
+    
+    $cartItem = Cart::where('product_id', $validated['product_id'])
+                    ->where('user_id', $request->user_id) 
+                    ->first();
+
+    if ($cartItem) {
+        $cartItem->quantity += $validated['quantity'];
+        $cartItem->save(); 
+    } else {
+        Cart::create([
+            'product_id' => $validated['product_id'],
+            'quantity' => $validated['quantity'],
+            'user_id' => $request->user_id, 
         ]);
-
-        $user = auth()->user();
-
-        if (!$user) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-
-        $cart = $user->cartItems()->first();  
-
-        if (!$cart) {
-            
-            $cart = Cart::create([
-                'user_id' => $user->id,
-            ]);
-        }
-
-
-        $cartItem = $cart->products()->where('product_id', $validated['product_id'])->first();
-
-        if ($cartItem) {
-            $cartItem->pivot->quantity += $validated['quantity'];
-            $cartItem->pivot->save();
-        } else {
-            $cart->products()->attach($validated['product_id'], ['quantity' => $validated['quantity']]);
-        }
-
-        return response()->json(['message' => 'Product added to cart successfully.'], 200);
     }
+ 
+    return response()->json(['message' => 'Product added to cart successfully.'], 200);
+}
+    
 }
