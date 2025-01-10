be2f93ec6e53285d10bbe73a285d2a0ab1bad6c6
Ibrahem <theblackchalnger@gmail.com>
2025-01-10 19:48:41 +0300

    0.2

--- routes/api.php
+++ routes/api.php
@@ -26,11 +26,8 @@ Route::post('/register_complement', [UserInfo::class, 'registerComp']);
 
 // Store routes
 Route::post('/create_store', [StoreController::class, 'createStore']);
-<<<<<<< HEAD
-=======
 Route::get('/show_all_stores', [StoreController::class, 'showAllStores']);
 Route::post('/show_stores_type', [StoreController::class, 'showStoresType']);// انت بتعطيه النوع و هو برجع الستورز من نفس النوع 
->>>>>>> 61e584144770b90f440b788db56c9f2ffb2df898
 
 // Products routes
 Route::post('/create_product', [ProductController::class, 'createProduct']);
@@ -42,7 +39,6 @@ Route::post('/show_product_by_store', [ProductController::class, 'ShowProductByS
 
 // Cart routes
 Route::post('add_to_cart', [CartController::class, 'addToCart']);
-<<<<<<< HEAD
 Route::post('/update_order', [CartController::class, 'updateOrder']);
 Route::post('/delete_order', [CartController::class, 'deleteOrder']);
 Route::post('/get_orders', [CartController::class, 'getOrders']);
@@ -52,7 +48,6 @@ Route::post('/add_to_favourite', [UserInfo::class, 'addToFav']);
 Route::post('/remove_from_favourite', [UserInfo::class, 'removeFav']);
 Route::post('/favourite_products', [UserInfo::class, 'getAllFavProducts']);
 Route::post('/favourite_stores', [UserInfo::class, 'getAllFavStores']);
-=======
 
 
 // ratings routes
@@ -60,4 +55,3 @@ Route::post('/product_rating', [ProductRatingController1::class, 'store'])->midd
 Route::post('/store_rating', [StoreRatingController::class, 'storeing'])->middleware('auth:sanctum');
 Route::post('/destroy_rating', [ProductRatingController1::class, 'destroy'])->middleware('auth:sanctum');
 
->>>>>>> 61e584144770b90f440b788db56c9f2ffb2df898
